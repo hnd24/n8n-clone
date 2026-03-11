e4c5b903e5fa5917eaa40cca1124c11e9ac0004a
hnd24 <duynhut366@gmail.com>
2026-03-12 03:54:02 +0700

    feat: initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Toaster } from 'sonner'
 import './index.css'
+import './globals.css'
 import App from './App.tsx'
 
 const queryClient = new QueryClient({
